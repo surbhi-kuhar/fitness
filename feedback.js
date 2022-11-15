e55ff62720d21386dd514d91bf988b80ae8c52b6
surbhi-kuhar <surbhikuhar07@gmail.com>
2022-11-15 22:55:19 +0530

    initial commit

---
+++ feedback.js
@@ -0,0 +1,3 @@
+function submit(){
+    alert("Thank You for your feedback.")
+}
---
+++ subs.js
@@ -0,0 +1,22 @@
+
+function three() {
+    document.getElementById("js1").innerHTML = 3000;
+
+}
+
+
+function six() {
+    let charges=6000;                                 //1000 per month for 6 months.
+    let discountedamount=6000-(10/100)*6000;          //discounted amount.
+    document.getElementById("js2").innerHTML=discountedamount;
+
+
+}
+
+function nine() {
+    let charges=9000;                                  //1000 per month for 9 months.  
+    let discountedamount=9000-(20/100)*9000;           //disounted amount.
+    document.getElementById("js3").innerHTML=discountedamount;
+
+
+}
